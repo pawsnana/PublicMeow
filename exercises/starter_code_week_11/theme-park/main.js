745929861aab3e084f6d19a7055084d7a12c317b
Tina Tsai <144840829+pawsnana@users.noreply.github.com>
2023-11-14 21:57:20 -0500

    in class execise

---
+++ exercises/starter_code_week_11/theme-park/main.js
@@ -0,0 +1,15 @@
+console.log('this works?')
+
+const age = 93;
+const height = 5;
+
+console.log(age)
+console.log(height)
+
+if (age < 8){
+    console.log('Check out the Meery-Go-Round')
+} else if (age > 8 && < 64 && height > 4.5) {
+    console.log('check out the roller coaster')
+} else {
+    console.log('why not take a float down teh lezy river')
+}
