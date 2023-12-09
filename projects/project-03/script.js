fd02c343a20dbe55c70cdcc40fe458327809fa80
Tina Tsai <144840829+pawsnana@users.noreply.github.com>
2023-12-09 11:22:16 -0500

    need to fix the icons.....still

--- projects/project-03/script.js
+++ projects/project-03/script.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const background = document.getElementById('background');
-    let isDragging = false;
+    let isDraggingBackground = false;
     let offsetX, offsetY;
 
-    function startDrag(e) {
-        isDragging = true;
+    function startDragBackground(e) {
+        isDraggingBackground = true;
         let clientX = e.clientX || e.touches[0].clientX;
         let clientY = e.clientY || e.touches[0].clientY;
-        offsetX = clientX - background.offsetLeft; // ....try to make the viewport start point here idk ???? hope thiss workkkk plz 
-        // okay trial 2 - using + insteadd of -...otherwise it jumped backt to very top-left corner Q___Q
-        // trial 3 not that complicated just this one?
+        offsetX = clientX - background.offsetLeft;
         offsetY = clientY - background.offsetTop; 
     }
 
-    function doDrag(e) {
-        if (isDragging) {
+    function doDragBackground(e) {
+        if (isDraggingBackground) {
             let clientX = e.clientX || (e.touches ? e.touches[0].clientX : 0);
             let clientY = e.clientY || (e.touches ? e.touches[0].clientY : 0);
             let newX = clientX - offsetX;
@@ -25,20 +23,101 @@ document.addEventListener("DOMContentLoaded", function() {
 
             background.style.left = newX + 'px';
             background.style.top = newY + 'px';
+
+            // to make the icons looks like floating trial 1
+            updateIconPositions(newX, newY);
         }
     }
 
-    function endDrag() {
-        isDragging = false;
+    function endDragBackground() {
+        isDraggingBackground = false;
     }
 
-    // for mouse 
-    background.addEventListener('mousedown', startDrag, true);
-    document.addEventListener('mouseup', endDrag, true);
-    document.addEventListener('mousemove', doDrag, true);
+    // this function is to update icon position when i move the background
+    function updateIconPositions(backgroundX, backgroundY) {
+        let icons = document.querySelectorAll('.icon'); 
+        icons.forEach(icon => {
+            let deltaX = backgroundX - parseInt(icon.getAttribute('data-initial-x') || 0);
+            let deltaY = backgroundY - parseInt(icon.getAttribute('data-initial-y') || 0);
 
-    // for touch 
-    background.addEventListener('touchstart', startDrag, true);
-    document.addEventListener('touchend', endDrag, true);
-    document.addEventListener('touchmove', doDrag, true);
+            let iconNewX = deltaX * 0.05; 
+            let iconNewY = deltaY * 0.05; 
+
+            icon.style.transform = `translate(${iconNewX}px, ${iconNewY}px)`;
+        });
+    }
+
+    // idk how to fix these Q___Q crying ....
+    let icons = document.querySelectorAll('.icon');
+    icons.forEach(icon => {
+        icon.setAttribute('data-initial-x', 0);
+        icon.setAttribute('data-initial-y', 0);
+    });
+
+    background.addEventListener('mousedown', startDragBackground, true);
+    document.addEventListener('mouseup', endDragBackground, true);
+    document.addEventListener('mousemove', doDragBackground, true);
+    background.addEventListener('touchstart', startDragBackground, true);
+    document.addEventListener('touchend', endDragBackground, true);
+    document.addEventListener('touchmove', doDragBackground, true);
+
+    // the icons' pick-and-drop function
+    icons = document.querySelectorAll('.icon');
+    let dropTargets = document.querySelectorAll('.division, #navbar');
+
+    function pickIcon(e) {
+        e.dataTransfer.setData('text', e.target.id);
+        e.target.style.position = 'absolute';
+    }
+
+    function dragIconOver(e) {
+        e.preventDefault();
+    }
+
+    function dropIcon(e) {
+        e.preventDefault();
+        let iconId = e.dataTransfer.getData('text');
+        let icon = document.getElementById(iconId);
+
+        const divisionRect = e.target.getBoundingClientRect();
+        const iconX = e.clientX - divisionRect.left;
+        const iconY = e.clientY - divisionRect.top;
+
+        icon.style.left = iconX + 'px';
+        icon.style.top = iconY + 'px';
+        e.target.appendChild(icon);
+
+        checkCompletion(e.target);
+    }
+
+    icons.forEach(icon => {
+        icon.addEventListener('dragstart', pickIcon);
+    });
+
+    dropTargets.forEach(target => {
+        target.addEventListener('dragover', dragIconOver);
+        target.addEventListener('drop', dropIcon);
+    });
+
+    function checkCompletion(division) {
+        let iconsInDivision = division.querySelectorAll('.icon');
+        let divisionId = division.id;
+        let correctIconsForDivisions = {
+            'div1_TD': ['icon_akali', 'icon_ekko', 'icon_senna'],
+            'div2_K': ['icon_akali', 'icon_ahri', 'icon_kaisa'],
+            
+        };
+
+        let correctIcons = correctIconsForDivisions[divisionId] || [];
+        let correctCount = 0;
+        iconsInDivision.forEach(icon => {
+            if (correctIcons.includes(icon.id)) {
+                correctCount++;
+            }
+        });
+
+        if (correctCount >= 3) {
+            division.style.backgroundImage = 'url(backgrounds/TD_1.png)';
+        }
+    }
 });
